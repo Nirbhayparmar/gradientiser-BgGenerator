a4d974003e13d988024fdf9eaf1feab8c8973034
Nirbhay Parmar <niravnirbhay94@gmail.com>
2021-03-27 20:26:37 +0530

    refector code for new feature

--- backgroundgen.js
+++ backgroundgen.js
@@ -1,28 +1,42 @@
-var color1 = document.getElementById("c1");
-var color2 = document.getElementById("c2");
-var css = document.getElementById("code");
-var body = document.getElementById("gradient");
-var button = document.getElementById("random");
-// var left = document.getElementById("left");
-// var right = document.getElementById("right");
-// a direction option is to be added by 20 fabruary.
+let color1 = document.getElementById("c1");
+let color2 = document.getElementById("c2");
+let css = document.getElementById("code");
+let body = document.getElementById("gradient");
+let button = document.getElementById("random");
+let radioGroup = document.getElementById("radioIn");
+let radioButton = radioGroup.getElementsByTagName("input");
+let direction;
 
+//the event listner is to be added.
+for (let x = 0; x < radioButton.length; x++) {
+  radioButton[x].addEventListener("input", radioValue);
+}
+
+function radioValue() {
+  for (let j = 0; j < radioButton.length; j++) {
+    if (radioButton[j].checked) {
+      direction = radioButton[j].value;
+      //console.log(direction);
+    }
+  }
+}
 function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
   return color;
 }
 
 function setgradient() {
   body.style.background =
-    "linear-gradient(to right , " + color1.value + ", " + color2.value + ")";
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
   css.textContent = body.style.background + ";";
+  console.log(body.style.background);
 }
 
 function randomgradient() {
-  var randomColor1 = getRandomColor();
-  var randomColor2 = getRandomColor();
+  let randomColor1 = getRandomColor();
+  let randomColor2 = getRandomColor();
   color1.value = randomColor1;
   color2.value = randomColor2;
   body.style.background =
@@ -33,4 +47,5 @@ function randomgradient() {
 color1.addEventListener("input", setgradient);
 color2.addEventListener("input", setgradient);
 button.addEventListener("click", randomgradient);
+
 window.onload = randomgradient();
