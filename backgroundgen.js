a62463a97574322a14cf234726f2396220f3760b
Naman <parmarnaman19@gmail.com>
2022-09-24 18:09:07 +0530

    updated

--- backgroundgen.js
+++ backgroundgen.js
@@ -9,46 +9,45 @@ let direction;
 
 //the event listner is to be added.
 for (let x = 0; x < radioButton.length; x++) {
-  radioButton[x].addEventListener("input", radioValue);
+	radioButton[x].addEventListener("input", radioValue);
 }
 
 function radioValue() {
-  for (let j = 0; j < radioButton.length; j++) {
-    if (radioButton[j].checked) {
-      direction = radioButton[j].value;
-      //console.log(direction);
-    }
-  }
+	for (let j = 0; j < radioButton.length; j++) {
+		if (radioButton[j].checked) {
+			direction = radioButton[j].value;
+			//console.log(direction);
+		}
+	}
 }
 function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
-  return color;
+	let letters = "0123456789ABCDEF";
+	let color = "#";
+	for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
+	return color;
 }
 
 function setgradient() {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-  css.textContent = body.style.background + ";";
-  console.log(body.style.background);
+	body.style.background =
+		"linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	css.textContent = body.style.background + ";";
+	console.log(body.style.background);
 }
 
 function randomgradient() {
-  let randomColor1 = getRandomColor();
-  let randomColor2 = getRandomColor();
+	let randomColor1 = getRandomColor();
+	let randomColor2 = getRandomColor();
 
-  while(randomColor1==randomColor2)
-  {
-     randomColor1 = getRandomColor();
-     randomColor2 = getRandomColor();
-  }
+	while (randomColor1 == randomColor2) {
+		randomColor1 = getRandomColor();
+		randomColor2 = getRandomColor();
+	}
 
-  color1.value = randomColor1;
-  color2.value = randomColor2;
-  body.style.background =
-    "linear-gradient(to right, " + randomColor1 + ", " + randomColor2 + ")";
-  css.textContent = body.style.background + ";";
+	color1.value = randomColor1;
+	color2.value = randomColor2;
+	body.style.background =
+		"linear-gradient(to right, " + randomColor1 + ", " + randomColor2 + ")";
+	css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setgradient);
