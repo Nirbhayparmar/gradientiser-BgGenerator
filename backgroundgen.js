5ca8f37a0de330ffa6f1cd7471e9bfec2e617d1b
Ebuka Ejiofor <ebuskelele@gmail.com>
2022-10-02 05:13:46 +0100

    Fix typos, change variable keywords from let to const, replace string concatenation with template strings
    Fix typo errors in README and contributing markdown files. Change keywords of variables assigned to DOM elements from let to const. Replace string concatenation with ES6 template strings

--- backgroundgen.js
+++ backgroundgen.js
@@ -1,73 +1,60 @@
-let color1 = document.getElementById("c1");
-let color2 = document.getElementById("c2");
-let css = document.getElementById("code");
-let body = document.getElementById("gradient");
-let button = document.getElementById("random");
-let radioGroup = document.getElementById("radioIn");
-let radioButton = radioGroup.getElementsByTagName("input");
+// Get elements from the DOM
+const color1 = document.getElementById("c1");
+const color2 = document.getElementById("c2");
+const css = document.getElementById("code");
+const body = document.getElementById("gradient");
+const button = document.getElementById("random");
+const radioGroup = document.getElementById("radioIn");
+const radioButton = radioGroup.getElementsByTagName("input");
 let direction;
 const directions = ["0deg", "90deg", "180deg", "270deg"];
 
-//the event listner is to be added.
+//the event listener is to be added.
 for (let x = 0; x < radioButton.length; x++) {
-  radioButton[x].addEventListener("input", radioValue);
+	radioButton[x].addEventListener("input", radioValue);
 }
 
 function radioValue() {
-  for (let j = 0; j < radioButton.length; j++) {
-    if (radioButton[j].checked) {
-      direction = radioButton[j].value;
-      body.style.background =
-        "linear-gradient(" +
-        direction +
-        ", " +
-        color1.value +
-        ", " +
-        color2.value +
-        ")";
-      css.textContent = body.style.background + ";";
-    }
-  }
+	for (let j = 0; j < radioButton.length; j++) {
+		if (radioButton[j].checked) {
+			direction = radioButton[j].value;
+			body.style.background = `linear-gradient(${direction}, ${color1.value}, ${color2.value})`;
+			css.textContent = body.style.background + ";";
+		}
+	}
 }
 function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
-  return color;
+	let letters = "0123456789ABCDEF";
+	let color = "#";
+	for (let i = 0; i < 6; i++)
+		color += letters[Math.floor(Math.random() * 16)];
+	return color;
 }
 
-function setgradient() {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-  css.textContent = body.style.background + ";";
-  console.log(body.style.background);
+function setGradient() {
+	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+	css.textContent = `${body.style.background};`;
+	console.log(body.style.background);
 }
 
-function randomgradient() {
-  let randomColor1 = getRandomColor();
-  let randomColor2 = getRandomColor();
+function randomGradient() {
+	let randomColor1 = getRandomColor();
+	let randomColor2 = getRandomColor();
 
-  while (randomColor1 == randomColor2) {
-    randomColor1 = getRandomColor();
-    randomColor2 = getRandomColor();
-  }
+	while (randomColor1 == randomColor2) {
+		randomColor1 = getRandomColor();
+		randomColor2 = getRandomColor();
+	}
 
-  color1.value = randomColor1;
-  color2.value = randomColor2;
-  let rand_direction = directions[Math.floor(Math.random() * 4)];
-  body.style.background =
-    "linear-gradient(" +
-    rand_direction +
-    ", " +
-    randomColor1 +
-    ", " +
-    randomColor2 +
-    ")";
-  css.textContent = body.style.background + ";";
+	color1.value = randomColor1;
+	color2.value = randomColor2;
+	let rand_direction = directions[Math.floor(Math.random() * 4)];
+	body.style.background = `linear-gradient(${rand_direction}, ${randomColor1}, ${randomColor2})`;
+	css.textContent = `${body.style.background};`;
 }
 
-color1.addEventListener("input", setgradient);
-color2.addEventListener("input", setgradient);
-button.addEventListener("click", randomgradient);
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+button.addEventListener("click", randomGradient);
 
-window.onload = randomgradient();
+window.onload = randomGradient();
